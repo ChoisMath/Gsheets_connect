66aa1d605e5a331467ed4611be03871588cec4aa
Chois <complete860127@gmail.com>
2024-05-03 20:31:35 +0900

    레디고

--- pages/4_부스운영학생 명단신청.py
+++ pages/4_부스운영학생 명단신청.py
@@ -6,13 +6,10 @@ from functions import *
 # Streamlit 앱 설정
 def main():
 
-    st.title('2024. 대수페_파일제출')
+    st.title('2024. 대수페_부스운영학생 명단제출')
 
-    st.write(""" 파일명은 [학교명_동아리명.hwp or 학교명_동아리명.hwpx]로 해주시기 바랍니다.  
-    수정된 파일을 업로드 할 떄도 파일명을 그대로 올려주시면,  
-    기존 파일은 삭제되고 새로운 파일이 업로드됩니다.
-    """)
-    data_upload_file()
+
+
 
 
 
